198d13c40deb79c02975077684c0586c59ee2617
Najas06 <najasneju@gmail.com>
2024-07-24 16:20:41 +0530

    Responsive

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ const darkMode = ()=>{
     const body = document.querySelector('body')
     if(body.style.backgroundColor == 'rgb(24, 24, 27)'){
         body.style.backgroundColor = 'white'
+        menu.style.backgroundColor = 'rgb(250 250 250 / var(--tw-bg-opacity)'
+        dashboard.style.color = 'rgb(24, 24, 27)'
     }
     else{
         body.style.backgroundColor = 'rgb(24, 24, 27)'
         const dashboard =   document.getElementById('dashboard')
         dashboard.style.color = 'white'
 
+        menu.style.backgroundColor = 'rgb(24, 24, 27)';
+
         const search = document.getElementById('search')
-        search.style.backgroundColor = 'white'
+        search.style.backgroundColor = 'white';
     }
 }
 
